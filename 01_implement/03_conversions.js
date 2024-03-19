faad979f20a83c24aff83ca7633d7e842fddcab4
Taiyab7 <161744570+Taiyab7@users.noreply.github.com>
2024-03-19 10:53:37 +0000

    conversions

---
+++ 01_implement/03_conversions.js
@@ -0,0 +1,12 @@
+
+
+let number = 33
+
+//let stringNumber = String(number)
+console.log(number)
+console.log(typeof(number))
+
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
+
